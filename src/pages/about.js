1e785f94d0b44cb0b532c2b4395e4b924534d91d
Gabriel C Moraru <gabrielcmoraru@gmail.com>
2018-09-06 00:44:08 +0100

    init

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class about extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>
+        Lorem ipsum dolor amet wolf man bun aesthetic vape, scenester woke la croix wayfarers. Butcher squid vegan twee. Coloring book swag helvetica microdosing bicycle rights franzen portland adaptogen activated charcoal la croix cliche brunch affogato. Adaptogen unicorn kogi, church-key tumeric craft beer neutra intelligentsia beard XOXO hella biodiesel yr. Gochujang succulents coloring book, hot chicken ennui messenger bag banjo.
+        Irony kitsch tousled lomo. 3 wolf moon meh tilde squid,
+        </p>
+      </div>
+    )
+  }
+}
